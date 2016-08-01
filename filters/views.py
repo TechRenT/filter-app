5fa57d87349bf63b9850663d678886df9e53899c
Renelle <titingrenelle@gmail.com>
2016-08-01 15:43:42 +0800

    Adding keywords list page

--- filters/views.py
+++ filters/views.py
@@ -1,3 +1,9 @@
-from django.http import render
+from django.http import HttpResponse
+
+from .models import Filter
 
 # Create your views here.
+def filter_keywords_list(request):
+    keywords = Filter.objects.all()
+    output = ", ".join([str(keyword) for keyword in keywords])
+    return HttpResponse(output)
