7c7d627d838d00df4f76fa813843a001d8284a6d
Renelle <titingrenelle@gmail.com>
2016-08-02 00:24:18 +0800

    Resolving layout.html problem

--- filter_site/settings.py
+++ filter_site/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'filter_site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates',],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
