3bd4a1ad0cd47783d16b13defacadad6fba61f08
Renelle <titingrenelle@gmail.com>
2016-08-01 10:58:49 +0800

    Start from scratch

--- filters/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Filter(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    keyword = models.CharField(max_length=255)
-    
-    def __str__(self):
-        return self.keyword
--- filters/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.filter_keywords_list, name='keywords'),
-    url(r'upload/$', views.upload_file, name='upload'),
-]
