4642d190e9c0f5fc4fa739ba56771b7ed88db22f
Renelle <titingrenelle@gmail.com>
2016-08-02 01:54:42 +0800

    Working on the main app

--- filter_site/urls.py
+++ filter_site/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
@@ -22,3 +25,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('filters.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- filters/urls.py
+++ filters/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^$', views.upload_file, name='main_app'),
     url(r'^keywords/$', views.filter_keywords_list, name='keywords'),
 ]
