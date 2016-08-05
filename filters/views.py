410a8b1462406f3901993c6dd621a20d4fe5a77e
Renelle <titingrenelle@gmail.com>
2016-08-05 15:31:15 +0800

    Improve the filter function and upload.html

--- filters/views.py
+++ filters/views.py
@@ -21,20 +21,32 @@ def csv_reader(filename, list):
         for row in read_csv:
             list.append(row)
 
-def filter(refine_raw_urls, keywords, bad_urls, good_urls):
-    """Check each raw url if it contains bad keywords.
-    If it has, the raw url will be save to bad_urls list.
-    Otherwise it will be added to good_urls list"""
-    for url in refine_raw_urls:
-        while True:
+#def filter(refine_raw_urls, keywords, bad_urls, good_urls):
+#    """Check each raw url if it contains bad keywords.
+#    If it has, the raw url will be save to bad_urls list.
+#    Otherwise it will be added to good_urls list"""
+#    for url in refine_raw_urls:
+#        while True:
+#            for keyword in keywords:
+#                if keyword in url[0]:
+#                    bad_urls.append(url)
+#                    break
+#            if url not in bad_urls:
+#                good_urls.append(url)
+#            break
+
+def filter(raw_urls, keywords, bad_urls, good_urls):
+    """Check each raw url whether it's good or bad"""
+    for url in raw_urls:
+        if url[2] == "200 OK" and int(url[3]) >= 3:
             for keyword in keywords:
                 if keyword in url[0]:
                     bad_urls.append(url)
                     break
             if url not in bad_urls:
                 good_urls.append(url)
-            break
-
+        else:
+            bad_urls.append(url)
 
 def save_csv(filename, list):
     """Save raw urls into a CSV file"""
@@ -53,15 +65,15 @@ def upload_file(request):
             good_urls = []
             keywords = [str(keyword) for keyword in Filter.objects.all()]
             csv_reader('media/raw_urls.csv', raw_urls)
-            refine_raw_urls = [url for url in raw_urls if url[2] == "200 OK"
-                       and int(url[3]) >= 3]
-            filter(refine_raw_urls, keywords, bad_urls, good_urls)
+#            refine_raw_urls = [url for url in raw_urls if url[2] == "200 OK"
+#                       and int(url[3]) >= 3]
+#            filter(refine_raw_urls, keywords, bad_urls, good_urls)
+            filter(raw_urls, keywords, bad_urls, good_urls)
             save_csv('media/cleanurls.csv', good_urls)
             save_csv('media/badurls.csv', bad_urls)
             return render(request, 'filters/upload.html', {'form': form,
                                                            'raw_urls': raw_urls,
                                                            'good_urls': good_urls,
-                                                           'refine_raw_urls': refine_raw_urls,
                                                            'bad_urls': bad_urls,
                                                            'keywords': keywords})
     else:
