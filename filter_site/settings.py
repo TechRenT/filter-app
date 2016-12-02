08149397d3137bcb10f189dd211ade48298a49f2
Renelle Tigue <titingrenelle@gmail.com>
2016-12-02 11:41:09 +0800

    install and set-up django-import-export

--- filter_site/settings.py
+++ filter_site/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'widget_tweaks',
+    'import_export',
     'accounts',
     'filters',
 ]
--- filters/admin.py
+++ filters/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 
 from .models import Filter
 
-# Register your models here.
-admin.site.register(Filter)
+
+class KeywordResource(resources.ModelResource):
+
+    class Meta:
+        model = Filter
+        fields = ('id', 'order', 'keyword')
+
+
+@admin.register(Filter)
+class KeywordAdmin(ImportExportModelAdmin):
+    resource_class = KeywordResource
